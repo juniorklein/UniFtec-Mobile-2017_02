fd414c309c0d9a71186fca39200bd13bc3d738aa
Mário Klein <mario@smartlifeapp.com.br>
2017-10-05 21:34:12 -0300

    ** Implementação aula 05/10

--- FtecListView/app/src/main/java/br/com/uniftec/fteclistview/adapter/FilmeAdapter.java
+++ FtecListView/app/src/main/java/br/com/uniftec/fteclistview/adapter/FilmeAdapter.java
@@ -1,6 +1,7 @@
 package br.com.uniftec.fteclistview.adapter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,12 +23,15 @@ import br.com.uniftec.fteclistview.model.Filme;
  * Created by marioklein on 21/09/17.
  */
 
-public class FilmeAdapter extends ArrayAdapter<Filme> {
+public class FilmeAdapter extends ArrayAdapter<Filme> implements View.OnClickListener{
 
     private LayoutInflater layoutInflater;
+    private SharedPreferences preferences;
 
     public FilmeAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Filme> objects) {
         super(context, resource, objects);
+
+        preferences = context.getSharedPreferences("FilmePreferences", Context.MODE_PRIVATE);
 
         layoutInflater = LayoutInflater.from(context);
     }
@@ -42,11 +47,16 @@ public class FilmeAdapter extends ArrayAdapter<Filme> {
 
         ImageView imagem = (ImageView)convertView.findViewById(R.id.row_list_view_filme_imagem);
         TextView textView = (TextView)convertView.findViewById(R.id.row_list_view_filme_titulo);
+        Button favoritar = (Button)convertView.findViewById(R.id.row_list_view_filme_favoritar);
 
         Filme filme = getItem(position);
+        boolean favorito = preferences.getBoolean(filme.getTitulo(), false);
 
         textView.setText(filme.getTitulo());
         int idImagem = getContext().getResources().getIdentifier(filme.getImagem(), "drawable", getContext().getPackageName());
+        favoritar.setTag(position);
+        favoritar.setOnClickListener(this);
+        favoritar.setBackgroundResource(favorito ? R.drawable.botao_favoritar_preenchido : R.drawable.botao_favoritar_vazado);
 
         try {
             imagem.setImageDrawable(getContext().getDrawable(idImagem));
@@ -58,5 +68,20 @@ public class FilmeAdapter extends ArrayAdapter<Filme> {
         return convertView;
     }
 
-    
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.row_list_view_filme_favoritar){
+            Filme filme = getItem((Integer)view.getTag());
+
+            boolean favorito = preferences.getBoolean(filme.getTitulo(), false);
+            favorito = !favorito;
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean(filme.getTitulo(), favorito);
+            editor.commit();
+
+            notifyDataSetChanged();
+
+        }
+    }
 }
